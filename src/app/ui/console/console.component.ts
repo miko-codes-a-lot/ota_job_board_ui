78cc697a22c3bbb655848b8cb1e4a31fbc6de18e
Miko Chu <miko.codecrafters@gmail.com>
2024-05-12 02:56:18 +0800

    add logout in console page

--- src/app/ui/console/console.component.ts
+++ src/app/ui/console/console.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-console',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './console.component.css'
 })
 export class ConsoleComponent {
+  constructor(private keycloakService: KeycloakService) {}
 
+  logout() {
+    this.keycloakService.logout(window.location.origin);
+  }
 }
