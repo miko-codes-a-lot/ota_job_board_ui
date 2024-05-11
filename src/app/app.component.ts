de14aff9d775d48a3fe04be3f6cad15dd26ffed5
Miko Chu <miko.codecrafters@gmail.com>
2024-05-12 02:56:04 +0800

    check if user logged in for testing purposes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { KeycloakProfile } from 'keycloak-js';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ota_jboard_ui';
+
+  profile?: KeycloakProfile;
+
+  constructor(private keycloakService: KeycloakService) {
+  }
+
+  ngOnInit(): void {
+    if (this.keycloakService.isLoggedIn()) {
+      this.keycloakService.loadUserProfile().then(profile => {
+        this.profile = profile;
+        console.log(this.profile, 'profile')
+      })
+    }
+  }
 }
